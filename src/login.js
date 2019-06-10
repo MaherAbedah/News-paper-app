420c38c151503daaca89cd5c391900e6e2407ab2
Maher-Abedah <m.abedah1@gmail.com>
2019-06-10 14:42:03 +0300

    feat: adding the CashID login button

---
+++ src/login.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import CashId from 'react-cashid'
+import './App.css';
+
+class Login extends Component {
+	render(){	
+		return (
+			<div>
+				<CashId
+				    domain="domain.com"
+				    path="/api/test"
+				    action="login"
+				    data="newsletter"
+				    metadata={{
+				      required: {
+				        identity: ['name', 'family'],
+				        position: ['country'],
+				        contact: ['email']
+				      },
+				      optional: {
+				        identity: ['age', 'gender'],
+				        position: ['city']
+				      }
+				    }}
+				    color="blue"
+				    qr={false}
+		    	/>
+		    </div>
+	    )
+	}
+}
+
+export default Login
