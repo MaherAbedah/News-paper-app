2ff71879baad874e9289b1131db3ccf661fdd521
Maher-Abedah <m.abedah1@gmail.com>
2019-06-21 18:54:49 +0300

    func: adding paymentSuccess function to the monthly payment button component

--- src/components/monthlyPayment.js
+++ src/components/monthlyPayment.js
@@ -3,6 +3,18 @@ import { BadgerButton } from 'badger-components-react'
 
 const MonthlyPayment = props => {
   
+  const paymentSuccess = (txid) =>
+  {
+  fetch("/api/monthpaid",
+	{
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify({"txid": txid, "url": "url"})
+	}).then(res => console.log(res))  
+} 
 
   return (
   	<BadgerButton
@@ -12,7 +24,7 @@ const MonthlyPayment = props => {
         text="1 month subscription"
         coinType="SLP"
         tokenId="65a84381f20769f791258851f213b0e3543a6b567ff2c6f9a9c587af7b4ffc6e"
-        successFn={() => console.log('success example function called')}
+        successFn={paymentSuccess}
         failFn={() => console.log('fail example function called')}
       />
     )
