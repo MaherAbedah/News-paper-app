cab1536b7e48e55807852b2613d10806f11f5d57
Maher-Abedah <m.abedah1@gmail.com>
2019-08-16 11:03:01 +0300

    FIX: compatepelity issue with IE11

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import 'core-js';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import "babel-polyfill"
 import './App.css';
 
 import GuestDashboard from './AppBar';
