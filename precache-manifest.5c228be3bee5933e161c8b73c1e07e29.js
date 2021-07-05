8bfc2f83f8b65d4d9eac8113e29375c78ed71b5d
Maher Abedah <m.abedah1@gmail.com>
2021-07-05 19:53:16 +0300

    Updates

---
+++ precache-manifest.5c228be3bee5933e161c8b73c1e07e29.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065038f53ad35dc14971ffda64298aef",
+    "url": "/News-paper-app/index.html"
+  },
+  {
+    "revision": "c8b7dac8dc47aba25dde",
+    "url": "/News-paper-app/static/css/2.475c5765.chunk.css"
+  },
+  {
+    "revision": "29b476d5cb0282a01535",
+    "url": "/News-paper-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c8b7dac8dc47aba25dde",
+    "url": "/News-paper-app/static/js/2.ea8cdcdc.chunk.js"
+  },
+  {
+    "revision": "29b476d5cb0282a01535",
+    "url": "/News-paper-app/static/js/main.b8e863bc.chunk.js"
+  },
+  {
+    "revision": "ff7e2ed3eb86d8287651",
+    "url": "/News-paper-app/static/js/runtime~main.a0c77757.js"
+  },
+  {
+    "revision": "5fdbe1eeac47f12733bad3c116e5adf7",
+    "url": "/News-paper-app/static/media/HS-logo.5fdbe1ee.png"
+  },
+  {
+    "revision": "26820f47448d9f01a9b696b738ca4a47",
+    "url": "/News-paper-app/static/media/Helsinginsanomat.26820f47.svg"
+  },
+  {
+    "revision": "77c15e58f8150230d536e165e3b01266",
+    "url": "/News-paper-app/static/media/Turunsanomat.77c15e58.svg"
+  },
+  {
+    "revision": "2c64554c7843410d16795d238296ba72",
+    "url": "/News-paper-app/static/media/image_five.2c64554c.jpg"
+  },
+  {
+    "revision": "5c01423c46196570b98b9031c04c3c1c",
+    "url": "/News-paper-app/static/media/image_five.5c01423c.jpg"
+  },
+  {
+    "revision": "9f2c9d0ef98c2cc6595920c1a3a94083",
+    "url": "/News-paper-app/static/media/image_five.9f2c9d0e.jpg"
+  },
+  {
+    "revision": "1be43fc86deffbddda96830485c1c678",
+    "url": "/News-paper-app/static/media/image_four.1be43fc8.jpg"
+  },
+  {
+    "revision": "72e8f41cd450f69fbaa7adb55bd8d529",
+    "url": "/News-paper-app/static/media/image_four.72e8f41c.jpg"
+  },
+  {
+    "revision": "beb4069c591611e55900e46df51b45f4",
+    "url": "/News-paper-app/static/media/image_four.beb4069c.jpg"
+  },
+  {
+    "revision": "211b0a4b8b4127831c81e754e1898f2f",
+    "url": "/News-paper-app/static/media/image_one.211b0a4b.jpg"
+  },
+  {
+    "revision": "b06bc3b3a816fb34cbb34aa849785677",
+    "url": "/News-paper-app/static/media/image_one.b06bc3b3.jpg"
+  },
+  {
+    "revision": "e291649c3d6817eff7c8d4685f6b507f",
+    "url": "/News-paper-app/static/media/image_one.e291649c.jpg"
+  },
+  {
+    "revision": "3cf4903475d666aa3bdda1bf1397ed38",
+    "url": "/News-paper-app/static/media/image_three.3cf49034.jpg"
+  },
+  {
+    "revision": "54624f89afba27c41e386608350224e5",
+    "url": "/News-paper-app/static/media/image_three.54624f89.jpg"
+  },
+  {
+    "revision": "bcb6cf9f17eed9f36b59213c926006f7",
+    "url": "/News-paper-app/static/media/image_three.bcb6cf9f.jpg"
+  },
+  {
+    "revision": "3e9142ed73f0200b942fe673aa4dbeb3",
+    "url": "/News-paper-app/static/media/image_two.3e9142ed.jpg"
+  },
+  {
+    "revision": "8a1857d066e72435043151f94a7661e4",
+    "url": "/News-paper-app/static/media/image_two.8a1857d0.jpg"
+  },
+  {
+    "revision": "b0c644468b2ab6e5c9a030e24eff3a81",
+    "url": "/News-paper-app/static/media/image_two.b0c64446.jpg"
+  },
+  {
+    "revision": "706ed2f1e73c2e5e43b5ede78b24cfc3",
+    "url": "/News-paper-app/static/media/kauppalehti.706ed2f1.svg"
+  },
+  {
+    "revision": "d6ee3ec17b81901d69b74d063435c2e5",
+    "url": "/News-paper-app/static/media/keskisuomalainen.d6ee3ec1.svg"
+  },
+  {
+    "revision": "4e8ac0a9d1f12c8cad79ffb2d4325d3b",
+    "url": "/News-paper-app/static/media/logo9.4e8ac0a9.png"
+  },
+  {
+    "revision": "5a0ca145cd59e75337e41bf74cdce1b9",
+    "url": "/News-paper-app/static/media/profile-placeholder.5a0ca145.png"
+  },
+  {
+    "revision": "8f41083c5d35eae20f71d0a5a0b2de6e",
+    "url": "/News-paper-app/static/media/savonsanomat.8f41083c.svg"
+  }
+]);
